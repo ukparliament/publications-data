d340832920f70ddb31afa54be31457c24a1cfc5b
James Jefferies <james@shedcode.co.uk>
2026-02-05 17:51:08 +0000

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,10 @@ Rails.application.routes.draw do
 
   get 'meta/cookies' => 'meta#cookies', as: :meta_cookies
 
-  get 'houses/:house/james-service' => 'concepts#index'
-
   Dataset.all_concept_types.each do |concept_types|
-    get "/#{concept_types}",      to: 'concepts#index', defaults: { concept_type: concept_types.underscore.camelize.singularize }
-    get "/#{concept_types}/:id",  to: 'concepts#show',  defaults: { concept_type: concept_types.underscore.camelize.singularize }, as: concept_types.singularize.underscore.to_sym
+    ap concept_types
+    get "/#{concept_types.underscore.downcase.pluralize}",      to: 'concepts#index', defaults: { concept_type: concept_types.underscore.camelize.singularize }
+    get "/#{concept_types.underscore.downcase.pluralize}/:id",  to: 'concepts#show',  defaults: { concept_type: concept_types.underscore.camelize.singularize }, as: concept_types.singularize.underscore.to_sym
   end
 
   Constants::ROUTE_OVERRIDES.each do |special|
