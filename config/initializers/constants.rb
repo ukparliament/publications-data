83d022ed8782b484773ecf1d3944230dd0a724bb
James Jefferies <james@shedcode.co.uk>
2026-02-05 17:44:35 +0000

    Publications routes

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,6 +3,15 @@ $TOGGLE_PORTCULLIS = ENV.fetch( "TOGGLE_PORTCULLIS", 'off' )
 $PROJECT_ID = ENV.fetch("PROJECT_ID")
 
 class Constants
-  CONCEPT_TITLE_MAP = {}
-  ROUTE_OVERRIDES= []
+  ROUTE_OVERRIDES = [
+    { route: "/houses/:house/research-services", to: 'concepts#index', concept_type: "House", property: "hasResearchProperty", as: :houses_research_services },
+    { route: "/houses/:house/publications", to: 'concepts#index', concept_type: "House", property: "hasResearchProperty", as: :houses_publications },
+    { route: "/publications", to: 'concepts#index', concept_type: "PublicationWork", as: :publications },
+    { route: "/publications/:id", to: 'concepts#show', concept_type: "PublicationWork", as: :publication },
+  ]
+
+
+  CONCEPT_TITLE_MAP = {
+    "PublicationWork" => "Publications"
+  }
 end
