19fb0e9a2402117ae1f448c4216c4e29737f707c
James Jefferies <james@shedcode.co.uk>
2026-02-12 09:57:57 +0000

    Adding placeholder and updated title for property routes

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -4,7 +4,7 @@ $PROJECT_ID = ENV.fetch("PROJECT_ID")
 
 class Constants
   ROUTE_OVERRIDES = [
-    { title: "Research services for a house", route: "/houses/:id/research-services", to: 'concepts#show', concept_type: "House", property: "hasResearchService", as: :houses_research_services },
+    { title: "Research services for the %{main_concept}", route: "/houses/:id/research-services", to: 'concepts#show', concept_type: "House", property: "hasResearchService", as: :houses_research_services },
     { route: "/houses/:id/publications", to: 'concepts#show', concept_type: "House", property: "hasResearchProperty", as: :houses_publications },
     { title: "Publications", route: "/publications", to: 'concepts#index', concept_type: "PublicationWork", as: :publications },
     { title: "Publication", route: "/publications/:id", to: 'concepts#show', concept_type: "PublicationWork", as: :publication },
