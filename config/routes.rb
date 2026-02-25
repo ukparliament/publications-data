2a214f4b8dec4ec5d5bfd5ff3e533a4033883728
James Jefferies <james@shedcode.co.uk>
2026-02-25 20:53:04 +0000

    Better handling of routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
   get 'meta/cookies' => 'meta#cookies', as: :meta_cookies
 
   Dataset.all_concept_type_routes.each do |concept_type_route|
-    get concept_type_route.nice_url_index_path, to: CONCEPTS_INDEX, defaults: { concept_type: concept_type_route.concept_type_for_defaults }
-    get concept_type_route.nice_url_show_path,  to: CONCEPTS_SHOW,  defaults: { concept_type: concept_type_route.concept_type_for_defaults }, as: concept_type_route.show_route_name
+    get concept_type_route.nice_url_index_path, to: ConceptTypeRoute::CONCEPTS_INDEX, defaults: { concept_type: concept_type_route.concept_type_for_defaults }
+    get concept_type_route.nice_url_show_path,  to: ConceptTypeRoute::CONCEPTS_SHOW,  defaults: { concept_type: concept_type_route.concept_type_for_defaults }, as: concept_type_route.show_route_name
   end
 
   Constants::ROUTE_OVERRIDES.each do |special|
