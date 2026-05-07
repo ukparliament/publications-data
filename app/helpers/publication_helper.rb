46770e7451aac67b757f5b974d1654f24b253a5a
James Jefferies <james@shedcode.co.uk>
2026-05-07 20:01:43 +0100

    Further tidying

--- app/helpers/publication_helper.rb
+++ app/helpers/publication_helper.rb
@@ -1,20 +1,4 @@
 module PublicationHelper
-  def title_of_publication_work_but_link_to_expression(publication_expression_datagraphs_id:, publication_work_datagraphs_id:, publication_expression_title: "Not found")
-    publication_work = Concept.find_by(datagraphs_id: publication_work_datagraphs_id)
-
-    label = if publication_work
-              publication_work.title
-            else
-              "Work not found for - #{publication_expression_title}"
-            end
-
-    link_to label, publication_path(publication_expression_datagraphs_id)
-  end
-
-  def contribution_is_public
-
-  end
-
   def person_details_and_role(person_details, role)
     name = person_details[1]
     id = person_details[0]
--- config/routes.rb
+++ config/routes.rb
@@ -14,15 +14,6 @@ Rails.application.routes.draw do
 
   get 'meta/cookies' => 'meta#cookies', as: :meta_cookies
 
-  # Dataset.all_concept_type_routes.each do |concept_type_route|
-  #   get concept_type_route.nice_url_index_path, to: ConceptTypeRoute::CONCEPTS_INDEX, defaults: { concept_type: concept_type_route.concept_type_for_defaults }
-  #   get concept_type_route.nice_url_show_path,  to: ConceptTypeRoute::CONCEPTS_SHOW,  defaults: { concept_type: concept_type_route.concept_type_for_defaults }, as: concept_type_route.show_route_name
-  # end
-
-  # Constants::ROUTE_OVERRIDES.each do |special|
-  #   get special[:route], to: special[:to], defaults: { concept_type: special[:concept_type], property: special[:property], title: special[:title] }, as: special[:as]
-  # end
-
   resources :houses, only: [:index, :show] do
     member do
       get 'research-services'
@@ -54,4 +45,3 @@ Rails.application.routes.draw do
   resources :people, only: [:index, :show]
   resources :users
 end
-
