234bfaed298b6c5f0b766af2bdc1e74f88989bff
James Jefferies <james@shedcode.co.uk>
2026-07-10 19:03:31 +0100

    Add split from

--- app/lib/datagraphs/api/get_publication.rb
+++ app/lib/datagraphs/api/get_publication.rb
@@ -10,6 +10,7 @@ module Datagraphs
         OPTIONAL MATCH (pw)-[r10:supersedes]->(superseded:PublicationWork)
         OPTIONAL MATCH (supersededBy:PublicationWork)-[r11:supersedes]->(pw:PublicationWork)
         OPTIONAL MATCH (pw)-[r12:mergedFrom]->(mf:PublicationWork)
+        OPTIONAL MATCH (pw)-[r13:splitFrom]->(sf:PublicationWork)
         WHERE pw.id='%{publication_work_id}'
         AND pes.label = 'Published'
         RETURN    pw.title AS title,
@@ -31,7 +32,9 @@ module Datagraphs
                   COLLECT_LIST(DISTINCT supersededBy.id) AS superseded_by_ids,
                   COLLECT_LIST(DISTINCT supersededBy.title) AS superseded_by_titles,
                   COLLECT_LIST(DISTINCT mf.id) AS merged_from_ids,
-                  COLLECT_LIST(DISTINCT mf.title) AS merged_from_titles
+                  COLLECT_LIST(DISTINCT mf.title) AS merged_from_titles,
+                  COLLECT_LIST(DISTINCT sf.id) AS split_from_ids,
+                  COLLECT_LIST(DISTINCT sf.title) AS split_from_titles
       Q
 
       PUBLICATION_ONLY = <<-Q.squish
