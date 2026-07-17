33b86f2cffb951404caa063ad70ddb872a32b98e
James Jefferies <james@shedcode.co.uk>
2026-07-17 21:15:21 +0100

    Add some temporary debug

--- app/lib/datagraphs/api/get_people.rb
+++ app/lib/datagraphs/api/get_people.rb
@@ -20,20 +20,21 @@ module Datagraphs
         LIMIT %{limit}
       Q
 
-      COUNT = <<-Q
+      COUNT = <<-Q.squish
         MATCH (p:Person)
         WHERE p.alphabetisationLetter = '%{letter}'
         RETURN count(p) AS total
       Q
 
-      LETTERS = <<-Q
+      LETTERS = <<-Q.squish
         MATCH res=(p:Person)
-        RETURN COLLECT(DISTINCT p.alphabetisationLetter) AS letters
+        RETURN COLLECT_LIST(DISTINCT p.alphabetisationLetter) AS letters
         ORDER BY letters asc
       Q
 
       def get_total(letter: 'A')
         params = { query: COUNT % { letter: letter } }
+        ap params
         response = call(params: params)
         output = JSON.parse(response.body)
         output["results"].first["total"]
@@ -43,14 +44,12 @@ module Datagraphs
         params = { query: QUERY % { skip: skip, limit: limit, letter: letter }}
         ap params
         response = call(params: params)
-        ap "HI"
-        ap response.body
-        ap "THERE"
         process_response(response.body)
       end
 
       def get_letters
         params = { query: LETTERS }
+        ap params
         response = call(params: params)
         process_response(response.body)
       end
