58b53cbe4cd1949b3342e382057b5958b677feea
James Jefferies <james@shedcode.co.uk>
2026-07-17 21:26:05 +0100

    Remove debug

--- app/lib/datagraphs/api/get_collections.rb
+++ app/lib/datagraphs/api/get_collections.rb
@@ -4,14 +4,15 @@ module Datagraphs
 
       QUERY = <<-Q.squish
         MATCH (c:Collection)-[r1:hasMember]->(pw:PublicationWork)
-        RETURN c.name AS name, count(pw) AS pw_count, c AS collection
+        RETURN c.name AS name,
+               COUNT(pw) AS pw_count,
+               c AS collection
       Q
 
       def process
         params = { query: QUERY }
-        ap params
+
         response = call(params: params)
-        ap response
         process_response(response.body)
       end
     end
