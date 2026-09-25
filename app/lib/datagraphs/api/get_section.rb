ce203f343a80f64306380a4f3c7a7c05f5acd8db
James Jefferies <james@shedcode.co.uk>
2026-09-25 12:20:35 +0100

    Add sections to a research service

--- app/lib/datagraphs/api/get_section.rb
+++ app/lib/datagraphs/api/get_section.rb
@@ -1,12 +1,16 @@
 module Datagraphs
   module Api
     class GetSection < CypherQuery
-      QUERY = <<-Q
-        MATCH (s:Section)
+      QUERY = <<-Q.squish
         MATCH (s:Section)-[r1:formsPartOf]->(rs:ResearchService)
-        WHERE
-          s.id = '%{section_id}'
-        RETURN s.id AS id, s.name AS name, s.shortName AS short_name, s.strapLine AS strap_line, s.isDefunct AS is_defunct, rs.id AS research_service_id, rs.name as research_service_name
+        WHERE s.id = '%{section_id}'
+        RETURN s.id AS id,
+        s.name AS name,
+        s.shortName AS short_name,
+        s.strapLine AS strap_line,
+        s.isDefunct AS is_defunct,
+        rs.id AS research_service_id,
+        rs.name as research_service_name
         LIMIT 1
       Q
 
@@ -14,7 +18,8 @@ module Datagraphs
         params = { query: QUERY % { section_id: section_id }}
 
         response = call(params: params)
-        process_response(response.body)
+        body = process_response(response.body)
+        Hashie::Mash.new(body.first)
       end
     end
   end
